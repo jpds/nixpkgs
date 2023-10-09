d9fb8694d67ac96b15323a71037d18e4484babf8
Anthony Roussel <anthony@roussel.dev>
2023-10-09 22:56:02 +0200

    thanos: 0.31.0 -> 0.32.4
    https://github.com/thanos-io/thanos/releases/tag/v0.32.0
    https://github.com/thanos-io/thanos/releases/tag/v0.32.1
    https://github.com/thanos-io/thanos/releases/tag/v0.32.2
    https://github.com/thanos-io/thanos/releases/tag/v0.32.3
    https://github.com/thanos-io/thanos/releases/tag/v0.32.4

--- pkgs/servers/monitoring/thanos/default.nix
+++ pkgs/servers/monitoring/thanos/default.nix
@@ -1,24 +1,21 @@
-{ lib, buildGoModule, fetchFromGitHub, fetchpatch }:
+{ lib
+, buildGoModule
+, fetchFromGitHub
+, go
+}:
+
 buildGoModule rec {
   pname = "thanos";
-  version = "0.31.0";
+  version = "0.32.4";
 
   src = fetchFromGitHub {
-    rev = "v${version}";
     owner = "thanos-io";
     repo = "thanos";
-    sha256 = "sha256-EJZGc4thu0WhVSSRolIRYg39S81Cgm+JHwpW5eE7mDc=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-EahgFeBYcjj4app+/XkcC8T99TvnrasjrtRphLthl8c=";
   };
 
-  patches = [
-    # https://github.com/thanos-io/thanos/pull/6126
-    (fetchpatch {
-      url = "https://github.com/thanos-io/thanos/commit/a4c218bd690259fc0c78fe67e0739bd33d38541e.patch";
-      hash = "sha256-Hxc1s5IXAyw01/o4JvOXuyYuOFy0+cBUv3OkRv4DCXs=";
-    })
-  ];
-
-  vendorHash = "sha256-8+MUMux6v/O2syVyTx758yUBfJkertzibz6yFB05nWk=";
+  vendorHash = "sha256-KM1TmTyi9gIHizL62MbzsmtJogI+oLuJ8K2lRb0MlpA=";
 
   doCheck = true;
 
@@ -30,11 +27,13 @@ buildGoModule rec {
     "-X ${t}.Branch=unknown"
     "-X ${t}.BuildUser=nix@nixpkgs"
     "-X ${t}.BuildDate=unknown"
+    "-X ${t}.GoVersion=${lib.getVersion go}"
   ];
 
   meta = with lib; {
     description = "Highly available Prometheus setup with long term storage capabilities";
     homepage = "https://github.com/thanos-io/thanos";
+    changelog = "https://github.com/thanos-io/thanos/releases/tag/v${version}";
     license = licenses.asl20;
     maintainers = with maintainers; [ basvandijk ];
   };
