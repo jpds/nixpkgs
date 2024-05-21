a8f265da45e327f8f3ab3a0f9b1f5ce98b6e3c52
Jonathan Davies <jpds@protonmail.com>
2024-05-22 00:32:19 +0100

    nixos/loki: Added test case for query journald string

--- nixos/tests/loki/standalone.nix
+++ nixos/tests/loki/standalone.nix
@@ -64,6 +64,10 @@ import ../make-test-python.nix (
       machine.wait_until_succeeds(
           "${pkgs.grafana-loki}/bin/logcli --addr='http://localhost:3100' query --no-labels '{job=\"varlogs\",filename=\"/var/log/testlog\"}' | grep -q 'Loki Ingestion Test'"
       )
+
+      machine.wait_until_succeeds(
+        "journalctl -o cat -u loki.service | grep 'executing query'"
+      )
     '';
   }
 )
